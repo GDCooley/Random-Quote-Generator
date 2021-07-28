c1a1a5c6ef46006f06b0095e721e85d37b67675b
GDCooley <gdcooley@gmail.com>
2021-07-28 18:26:27 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ project 1 - A Random Quote Generator
  * `quotes` array 
  ***/
 
+// The below is an array of objects containing the quotes for the overall program
+
 const quotes = [{
         quote: 'We live in a society exquisitely dependent on science and technology, in which hardly anyone knows anything about science and technology.',
         source: 'Carl Sagan'
@@ -25,7 +27,7 @@ const quotes = [{
         quote: "If cats looked like frogs we'd realize what nasty, cruel little bastards they are. Style. That's what people remember.",
         source: 'Terry Pratchett',
         year: '1992',
-        tags: ['fantasy', 'humor']
+        tags: ['fantasy', 'humor'] //Lucked out with this on first attempt using an array for multiple tags
     },
     {
         quote: 'Never let your sense of morals prevent you from doing what is right.',
@@ -44,8 +46,8 @@ const quotes = [{
 
 function getRandomQuote() {
     let num = Math.floor(Math.random() * quotes.length); //obtains a random number between 1 and the # of array values
-    console.log(num);
-    return quotes[num];
+    console.log(num); // For my own testing purposes to ensure a random number is generated
+    return quotes[num]; // Provides a value (one full object in this case) to the requesting portion of the program
 }
 
 /***
@@ -53,16 +55,19 @@ function getRandomQuote() {
  ***/
 
 function printQuote() {
-    let randomQuote = getRandomQuote();
-    let HTMLString = ``;
-    HTMLString += `<p class="quote">${randomQuote.quote}</p>`;
-    HTMLString += `<p class="source">${randomQuote.source}`;
+    let randomQuote = getRandomQuote(); // Calls, and assigns the returned value of, the getRandomQuote object to the variable
+    let HTMLString = ``; // Initiating an empty string that will be populated by the below code
+    HTMLString += `<p class="quote">${randomQuote.quote}</p>`; // Adds the quote to HTMLString
+    HTMLString += `<p class="source">${randomQuote.source}`; // Adds the source to HTMLString
+    // Below tests if the quote has a citation, and if so, adds it to HTMLString
     if (randomQuote.citation) {
         HTMLString += `<span class="citation">${randomQuote.citation}</span>`;
     }
+    // Below tests if the quote has a year, and if so, adds it to HTMLString
     if (randomQuote.year) {
         HTMLString += `<span class="year">${randomQuote.year}</span>`;
     }
+    // Below tests if the quote has any tags, and if so, adds it to HTMLString on its own line below the quote and source lines
     HTMLString += `</p>`;
     if (randomQuote.tags) {
         HTMLString += `<p class="tags">Tags: ${randomQuote.tags}</p>`
