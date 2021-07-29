68382df9e0af81b6efd3cac911b34390e0ac41a9
GDCooley <gdcooley@gmail.com>
2021-07-29 17:53:51 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ const quotes = [{
 
 function getRandomQuote() {
     const num = Math.floor(Math.random() * quotes.length);
-    console.log(num);
+    // console.log(num);
     return quotes[num];
 }
 
@@ -59,8 +59,8 @@ function getRandomQuote() {
 
 // This function prints a random quote to the web page.  It begins by calling the GetRandomQuote function which returns
 // the quote.  The function then concatenates the full HTML string that will be printed.  It always prints the quote and 
-// author.  It then determins if the quote also includes a citation, year, and or associated tags and if so adds
-// them to the HTML string.  Commented out at the bottom of the function was the first way I did this, ehich worked, but
+// author.  It then determines if the quote also includes a citation, year, and / or associated tags and if so adds
+// them to the HTML string.  Commented out at the bottom of the function was the first way I did this, which worked, but
 // did not match the requirements of the project.
 
 function printQuote() {
@@ -79,13 +79,18 @@ function printQuote() {
         HTMLString += `<p class="tags">Tags: ${randomQuote.tags}</p>`
     }
 
+    // This optional portion changes the background to a new color every time the a new quote is presented
+
     const red = Math.floor(Math.random() * 255);
     const green = Math.floor(Math.random() * 255);
     const blue = Math.floor(Math.random() * 255);
-
     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+    // This prints the resulting string to the web page in place of the previous quote
+
     document.querySelector('.quote-box').innerHTML = HTMLString;
 
+    // This was the alternate way I did it before rereading the requirements
     // document.querySelector('.quote').innerHTML = `${randomQuote.quote}`;
     // if (randomQuote.citation && randomQuote.year) {
     //     document.querySelector('.source').innerHTML = `${randomQuote.source}<span class="citation"></span><span class="year"></span>`;
